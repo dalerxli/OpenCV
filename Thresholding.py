ec9d8bf50405ee3620afb14ee7e2740cff969998
Rahul Kumar <29652757+rahuls321@users.noreply.github.com>
2017-12-15 13:24:46 +0530

    Update Thresholding.py

--- Thresholding.py
+++ Thresholding.py
@@ -5,12 +5,16 @@ import numpy as np
 img = cv2.imread('bookpage.jpg')
 
 retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
+
 #Convert into Gray color
 grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 #Now apply threshold on converted IMAGE
 retval2, threshold2 = cv2.threshold(grayscaled, 12, 255, cv2.THRESH_BINARY)
+
 #Able to read images of low weights 
 gaus = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+
 #OTSU another kind of THRESHOLDING -- Used depending on image weights.
 retval2, otsu = cv2.threshold(grayscaled, 125,255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
